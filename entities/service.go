d109a07e58d0b19efcefbef0b151bc3ca26fffd1
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-03-21 17:34:41 -0300

    feat: :sparkles: create service entity

---
+++ entities/service.go
@@ -0,0 +1,12 @@
+package entities
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Service struct {
+	Id primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name string	`bson:"name,omitempty" json:"name,omitempty"`
+	MinDuration int `bson:"min_duration,omitempty" json:"min_duration,omitempty"`
+	MinPrice float32 `bson:"min_price,omitempty" json:"min_price,omitempty"`
+}
