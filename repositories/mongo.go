7534e92ccf156a196e5186566aea405c8c430b2d
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-03-24 21:29:52 -0300

    refactor: make instance of company

--- repositories/mongo.go
+++ repositories/mongo.go
@@ -4,20 +4,43 @@ import (
 	"context"
 	"time"
 
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var client *mongo.Client
+type Repository struct {
+	Client *mongo.Client
+	Ctx context.Context
+	CtxCancel context.CancelFunc
+	ClientOptions *options.ClientOptions
+}
 
-func MongoConnection () (*mongo.Client, context.CancelFunc) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10 * time.Second)
-	var err error
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+func New() *Repository {
+	logrus.Warn("Creating new repository")
+	ctx, cancel := context.WithTimeout(context.Background(), 2 * time.Second)
+	options := options.Client().ApplyURI("mongodb+srv://omicronrpl:oIe3hKERFNXPtiyF@hsa.sqnczja.mongodb.net/?authSource=admin")
+	client, err := mongo.Connect(ctx, options)
 
 	if err != nil {
+		logrus.Fatal(err)
 		panic(err)
 	}
 
-	return client, cancel
+	defer logrus.Info("Repository Created")
+
+	return &Repository{
+		Client: client,
+		Ctx: ctx,
+		CtxCancel: cancel,
+		ClientOptions: options,
+	}
+}
+
+func (r *Repository) Ping() {
+	logrus.Warn("Pinging MongoDB ...")
+	if err := r.Client.Ping(r.Ctx, r.ClientOptions.ReadPreference); err != nil {
+		logrus.Fatal("Mongo Database Unavailable")
+		panic(err)
+	}
 }
